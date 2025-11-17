c8035c9da70d3f0daf349c70cd57a850cb12e52d
Philip J. <philjn@proton.me>
2025-11-17 23:04:34 +0100

    template: Add PART1 vs PART2 macro

--- 2020/template.c
+++ 2020/template.c
@@ -1,5 +1,12 @@
 #include "libpj.h"
 
+#if defined(PART1) && defined(PART2)
+#error "Define either PART1 or PART2, not both"
+#endif
+#if !defined(PART1) && !defined(PART2)
+#error "Define either PART1 or PART2"
+#endif
+
 int main(void) {
   String_Builder sb = {0};
   sb_read_file(&sb, stdin);
