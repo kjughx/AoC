2ec947988883419d8d52f1691923d6fe8b197987
Philip J. <philjn@proton.me>
2025-12-10 07:21:27 +0100

    Different algo, same result...

--- 2025/src/day9.c
+++ 2025/src/day9.c
@@ -17,69 +17,92 @@ typedef struct {
 } Polygon;
 
 typedef struct {
-  Vector2 start;
-  Vector2 end;
-} Line;
+  Vector2 A, B;
+  Vector2 min, max;
+} Rect;
 
-bool lines_intersect(Line* l1, Line *l2) {
-    // Calculate the coefficients of the line equations
-  Vector2 p1 = l1->start;
-  Vector2 p2 = l1->end;
-  Vector2 p3 = l2->start;
-  Vector2 p4 = l2->end;
+#define rect(a, b) {.A = (a), .B = (b),                     \
+    .min = {.x = MIN((a).x, (b).x), .y = MIN((a).y, (b).y)},  \
+    .max = {.x = MAX((a).x, (b).x), .y = MAX((a).y, (b).y)}}
 
-  int a1 = p2.y - p1.y;
-  int b1 = p1.x - p2.x;
-  int a2 = p4.y - p3.y;
-  int b2 = p3.x - p4.x;
+typedef struct {
+  Rect *items;
+  size_t count;
+  size_t capacity;
+} Rects;
 
-  int determinant = a1 * b2 - a2 * b1;
-  print(determinant);
-
-  return determinant != 0;
+bool rect_overlap(Rect *r, Rect *e) {
+  return !(r->max.x <= e->min.x ||
+           e->max.x <= e->min.x ||
+           r->max.y <= e->min.y ||
+           e->max.y <= e->min.y);
 }
 
-bool is_inside(Polygon *pg, Vector2 p) {
-  int c = 0;
+/* https://www.engr.colostate.edu/~dga/documents/papers/point_in_polygon.pdf */
+double winding(Polygon *pg, Vector2 p) {
+  double w = 0;
+
   for (size_t i = 0; i < pg->count; ++i) {
     Vector2 a = pg->items[i];
     Vector2 b = pg->items[(i + 1) % pg->count];
-    ssize_t min,max;
 
-    if (a.x == b.x) {
-      min = MIN(a.y, b.y);
-      max = MAX(a.y, b.y);
-      /* Trace a ray to the right, does it intersect? */
-      if (p.x <= a.x && min < p.y && p.y <= max) {
-        if (p.x == a.x) return true; /* If it's on the line, that counts */
-        c++;
+    Vector2 va = {.x = a.x - p.x, .y = a.y - p.y};
+    Vector2 vb = {.x = b.x - p.x, .y = b.y - p.y};
+
+    /* Point on segment? */
+    if (va.y * vb.y == 0) { /* Horizontal */
+      if (va.x * vb.x <= 0) return true;
+    }
+    if (va.x * vb.x == 0) { /* Vertical */
+      if (va.y * vb.y <= 0) return true;
+    }
+
+    if (va.y * vb.y < 0) {
+      double r = va.x + va.y * (vb.x - va.x) / (float)(va.y - vb.y);
+      if (r > 0) {
+        if (va.y < 0) w += 1.0f;
+        else          w -= 1.0f;
       }
-    } else if (a.y == b.y) {
-      min = MIN(a.x, b.x);
-      max = MAX(a.x, b.x);
-      if (p.y == a.y && min <= p.x && p.x <= max) return true; /* On the line */
+    } else if (va.y == 0 && va.x > 0) {
+      if (vb.y > 0) w += (1.0f / 2);
+      else          w -= (1.0f / 2);
+    } else if (vb.y == 0 && vb.x > 0) {
+      if (va.y < 0) w += (1.0f / 2);
+      else          w -= (1.0f / 2);
     }
   }
-  return c % 2;
+
+  return w;
 }
 
-void print_rect(Polygon *big, Polygon *small) {
-  Grid G = {.nx = 12, .ny = 12};
-  ma_init(&G);
-  ma_fill(&G, '.');
-  for (size_t i = 0; i < big->count; ++i) {
-    *ma_at(&G, big->items[i].x - 1, big->items[i].y -1 ) = '#';
-  }
-  for (size_t i = 0; i < small->count; ++i) {
-    *ma_at(&G, small->items[i].x - 1, small->items[i].y - 1) = 'O';
-  }
-  for (size_t y = 0; y < G.ny; ++y) {
-    for (size_t x = 0; x < G.nx; ++x) {
-      printf("%c", *ma_at(&G, x, y));
-    }
-    printf("\n");
-  }
-}
+/* ssize_t min, max; */
+/* bool is_inside(Polygon *pg, Vector2 p) { */
+/*   return winding(pg, p) != 0; */
+/* } */
+
+/* bool is_inside(Polygon *pg, Vector2 p) { */
+/*   int c = 0; */
+/*   for (size_t i = 0; i < pg->count; ++i) { */
+/*     Vector2 a = pg->items[i]; */
+/*     Vector2 b = pg->items[(i + 1) % pg->count]; */
+/*     ssize_t min,max; */
+
+/*     if (a.x == b.x) { */
+/*       min = MIN(a.y, b.y); */
+/*       max = MAX(a.y, b.y); */
+/*       /\* Trace a ray to the right, does it intersect? *\/ */
+/*       if (p.x <= a.x && min < p.y && p.y <= max) { */
+/*         if (p.x == a.x) return true; /\* If it's on the line, that counts *\/ */
+/*         c++; */
+/*       } */
+/*     } else if (a.y == b.y) { */
+/*       min = MIN(a.x, b.x); */
+/*       max = MAX(a.x, b.x); */
+/*       if (p.y == a.y && min <= p.x && p.x <= max) return true; /\* On the line *\/ */
+/*     } */
+/*   } */
+/*   return c % 2; */
+/* } */
 
 int main(void) {
   String_Builder sb = {0};
@@ -88,8 +111,8 @@ int main(void) {
 
   Polygon pg = {0};
   sb_foreach_line(&sb, line) {
-    u64 x,y;
-    expect(sscanf(line, "%ld,%ld", &x, &y) == 2);
+    ssize_t x,y;
+    expect(sscanf(line, "%zd, %zd", &x, &y) == 2);
     da_append(&pg, ((Vector2){x,y}));
   }
 
@@ -108,21 +131,63 @@ int main(void) {
 
 /* Counter clockwise? */
   u64 part2 = 0;
+
+  Rects edges = {0};
+  for (size_t i = 0; i < pg.count; ++i) {
+    Vector2 a = pg.items[i];
+    Vector2 b = pg.items[(i + 1) % pg.count];
+    Rect e = {
+      .A = a,
+      .B = b,
+      .min = {.x = MIN(a.x, b.x), .y = MIN(a.y, b.y)},
+      .max = {.x = MAX(a.x, b.x), .y = MAX(a.y, b.y)},
+    };
+    da_append(&edges, e);
+  }
   for (size_t i = 0; i < pg.count; ++i) {
     for (size_t j = 0; i != j && j < pg.count; ++j) {
       Vector2 A = pg.items[i];
       Vector2 C = pg.items[j];
       Vector2 B = (Vector2){.x = C.x, .y = A.y};
       Vector2 D = (Vector2){.x = A.x, .y = C.y};
-      u64 xx = A.x > C.x ? A.x - C.x : C.x - A.x;
-      u64 yy = A.y > C.y ? A.y - C.y : C.y - A.y;
-      u64 area = (xx + 1) * (yy + 1);
-      if (area > part2 &&
-          (is_inside(&pg, A) && is_inside(&pg, B) && is_inside(&pg, C) && is_inside(&pg, D))) {
-          part2 = area;
+
+      u64 area = (ABS(A.x - C.x) + 1) * (ABS(A.y - C.y) + 1);
+      Rect r = rect(A, C);
+      bool overlaps = false;
+      for (size_t k = 0; k < edges.count; ++k) {
+        if (rect_overlap(&r, &edges.items[k])) overlaps = true;
       }
+      if (!overlaps) part2 = MAX(area, part2);
     }
   }
+  /* for (size_t i = 0; i < pg.count; ++i) { */
+  /*   for (size_t j = 0; i != j && j < pg.count; ++j) { */
+  /*     Vector2 A = pg.items[i]; */
+  /*     Vector2 C = pg.items[j]; */
+  /*     Vector2 B = (Vector2){.x = C.x, .y = A.y}; */
+  /*     Vector2 D = (Vector2){.x = A.x, .y = C.y}; */
+
+  /*     u64 area = (ABS(A.x - C.x) + 1) * (ABS(A.y - C.y) + 1); */
+
+  /*     A.x += SIGN(B.x - A.x); */
+  /*     B.x -= SIGN(B.x - A.x); */
+  /*     C.x -= SIGN(B.x - A.x); */
+  /*     D.x += SIGN(B.x - A.x); */
+
+  /*     A.y += SIGN(D.y - A.y); */
+  /*     B.y -= SIGN(D.y - A.y); */
+  /*     C.y -= SIGN(D.y - A.y); */
+  /*     D.y += SIGN(D.y - A.y); */
+
+  /*     /\* u64 xx = A.x > C.x ? A.x - C.x : C.x - A.x; *\/ */
+  /*     /\* u64 yy = A.y > C.y ? A.y - C.y : C.y - A.y; *\/ */
+  /*     /\* u64 area = (xx + 1) * (yy + 1); *\/ */
+  /*     if (area > part2 && */
+  /*         (is_inside(&pg, A) && is_inside(&pg, B) && is_inside(&pg, C) && is_inside(&pg, D))) { */
+  /*         part2 = area; */
+  /*     } */
+  /*   } */
+  /* } */
 
 #ifdef TEST
   Grid G = {.nx = 15, .ny = 10};
@@ -134,9 +199,12 @@ int main(void) {
 
   for (size_t x = 0; x < G.nx; ++x) {
     for (size_t y = 0; y < G.ny; ++y) {
-      if (is_inside(&pg, (Vector2){x, y})) {
-        if (*ma_at(&G, x, y) != '#') *ma_at(&G, x, y) = 'O';
-      }
+      /* double w = winding(&pg, (Vector2){x, y}); */
+      /* if (w == 1.0f) *ma_at(&G, x, y) = 'O'; */
+      /* else if (w == 0.5) *ma_at(&G, x, y) = 'P'; */
+      /* else if (w == -0.5) *ma_at(&G, x, y) = 'N'; */
+      if (is_inside(&pg, (Vector2){x, y})) *ma_at(&G, x, y) = 'O';
+      /* if (*ma_at(&G, x, y) != '#') *ma_at(&G, x, y) = (int)winding(&pg, (Vector2){x, y}) + '0'; */
     }
   }
   for (size_t y = 0; y < G.ny; ++y) {
@@ -149,5 +217,7 @@ int main(void) {
   printf("part2 = %ld\n", part2);
 }
 /* 4733094760 too high */
+/* 4605266710 wrong */
 /* 4605538168 wrong */
+/* 3184727672 wrong */
 /* 1682024932 wrong */
